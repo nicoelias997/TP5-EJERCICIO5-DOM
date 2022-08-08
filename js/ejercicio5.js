3f8d7f568a14ba63986e71bb39e12b73c29e2af9
nicoelias997 <nicoelias997@gmail.com>
2022-08-08 14:27:09 +0200

    la funcionalidad existe, pero si aprieto 2 veces el boton iniciar, se acelera x2 y no puedo pausar, debo corregir

---
+++ js/ejercicio5.js
@@ -0,0 +1,47 @@
+const iniciar = document.getElementById("iniciar");
+const pausar = document.getElementById("pausar");
+const reset = document.getElementById("reset");
+const cronometro = document.getElementById("cronometro");
+
+let horas = 0;
+let minutos = 0;
+let segundos = 0;
+
+let intervalo;
+
+const iniciarCron = () => {
+
+intervalo = setInterval(() => {
+segundos++; //aumento segundos
+
+if(segundos == 60){ //agrego un minuto
+    minutos++
+    segundos = 0;
+}
+if(minutos == 60){ //agrego un minuto
+    horas++
+    minutos = 0;
+}
+
+cronometro.innerHTML = `
+<p>${horas}:${minutos}:${segundos} </p>
+
+`
+}
+,100)
+
+}
+
+const pausarCron = () => {
+    clearInterval(intervalo)
+}
+
+const resetCron = () => {
+    clearInterval(intervalo);
+    horas = "00"
+    minutos = "00"
+    segundos = "00"
+    cronometro.innerHTML = `
+<p>${horas}:${minutos}:${segundos}</p> 
+`
+}
